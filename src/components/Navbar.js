51b11209ff6f1c135f594ef55fa57ac237307d0e
playerony <pawel.wojtasinski.1995@gmail.com>
2018-05-26 21:02:08 +0200

    Create Navbar component with styles

---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import logo from '../assets/logo.png'
+
+import '../styles/Navbar.css'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <ul className="nav">
+        <li>
+          <Link to='/'>
+            <img className="logo" src={logo} alt='logo' />
+          </Link>
+        </li>
+        <li>
+          <Link className="text" to='/videoEditor'>
+            Videos
+          </Link>
+        </li>
+        <li>
+          <Link className="text" to='/videoConverter'>
+            Video converter
+          </Link>
+        </li>
+        <li>
+          <Link className="text" to='/memeGenerator'>
+            Meme generator
+          </Link>
+        </li>
+        <li>
+          <Link className="text" to='/photoEditor'>
+            Photo editor
+          </Link>
+        </li>
+        <li>
+          <Link className="text" to='/photoGallery'>
+            Gallery
+          </Link>
+        </li>
+      </ul>
+    )
+  }
+}
+export default Navbar
